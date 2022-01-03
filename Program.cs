d13e4bee0b30fb7c8b23606dd0a7d1585ba7fa8d
Lauren McCall <lmmccolloch@gmail.com>
2022-01-03 17:18:17 -0500

    Added PromptForString and PromptForInteger

--- Program.cs
+++ Program.cs
@@ -14,6 +14,31 @@ namespace FirstBankOfSuncoast
             Console.WriteLine("");
         }
 
+        public string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isthisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isthisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that is not a valid input. I'm using 0 as your answer. ");
+                return 0;
+            }
+        }
+
         static void Main(string[] args)
         {
             DisplayGreeting();
