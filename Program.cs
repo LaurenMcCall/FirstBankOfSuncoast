1151f69d0d12222cdb3e0edfbec3c76662796c8f
Lauren McCall <lmmccolloch@gmail.com>
2022-01-04 11:35:18 -0500

    Added Date to Transaction properties

--- Program.cs
+++ Program.cs
@@ -5,9 +5,10 @@ namespace FirstBankOfSuncoast
 {
     class Transaction
     {
+        public DateTime Date { get; set; }
         public string Account { get; set; }
-        public string Amount { get; set; }
         public string TransactionType { get; set; }
+        public int Amount { get; set; }
     }
 
     class Program
