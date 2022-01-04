79cc7efc02ba76c45d648de0a395fd762f993da5
Lauren McCall <lmmccolloch@gmail.com>
2022-01-04 15:47:52 -0500

    Deposit is working.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FirstBankOfSuncoast
 {
-    class Transaction
-    {
-        public DateTime Date { get; set; }
-        public string Account { get; set; }
-        public string TransactionType { get; set; }
-        public int Amount { get; set; }
-    }
-
     class Program
     {
         static void DisplayGreeting()
@@ -56,23 +49,46 @@ namespace FirstBankOfSuncoast
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("--------------------------------------");
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("(1) View Account Balances");
-            Console.WriteLine("(2) Deposit Funds");
-            Console.WriteLine("(3) Withdraw Funds");
-            Console.WriteLine("(4) View Transaction History");
-            Console.WriteLine("(5) Quit");
+            Console.WriteLine("[B]ALANCES");
+            Console.WriteLine("[D]EPOSIT");
+            Console.WriteLine("[W]ITHDRAW");
+            Console.WriteLine("[T]RANSACTION HISTORY");
+            Console.WriteLine("[Q]UIT");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("--------------------------------------");
             Console.WriteLine("");
-
-
         }
+
+        private static int ComputeCheckingBalance(List<Transaction> transactions)
+        {
+            var totalCheckingDeposits = transactions.Where(transaction => transaction.Account == "Checking" && transaction.Type == "Deposit")
+                                                    .Sum(transaction => transaction.Amount);
+            var totalCheckingWithdraw = transactions.Where(transaction => transaction.Account == "Checking" && transaction.Type == "Withdraw")
+                                                    .Sum(transaction => transaction.Amount);
+            var totalChecking = totalCheckingDeposits - totalCheckingWithdraw;
+            return totalChecking;
+        }
+
 
         static void Main(string[] args)
         {
             DisplayGreeting();
 
             var transactions = new List<Transaction>();
+            {
+                var firstCheckingDeposit = new Transaction()
+                {
+                    Amount = 10,
+                    Account = "Checking",
+                    Type = "Deposit"
+                };
+                var firstSavingsDeposit = new Transaction()
+                {
+                    Amount = 10,
+                    Account = "Savings",
+                    Type = "Deposit"
+                };
+            };
 
             var keepGoing = true;
 
@@ -84,28 +100,88 @@ namespace FirstBankOfSuncoast
 
                 switch (choice)
                 {
-                    case "1":
-
-                        // VIEW ACCOUNT BALANCES.
-                        // If user selects `deposit to savings`
-                        // - prompt user to input amount (PromptForInteger)
-                        // - add to savings history
-                        // - update balance
-                        // If user selects `deposit to checking`
-                        // - prompt user to input amount (PromptForInteger)
-                        // - add to checking history
-                        // - update balance
-                        // If user selects `withdraw from savings`
-                        // - prompt user to input amount (PromptForInteger)
-                        // - add amount to savings history
-                        // - update balance
-                        //   - if withdrawal would make balance < 0, display error message
-                        // If user selects `withdraw from checking`
-                        // - prompt user to input amount (PromptForInteger)
-                        // - add to checking history
-                        // - update balance
-                        //   - if withdrawal would make balance < 0, display error message
+                    case "B":
+                        int totalChecking = ComputeCheckingBalance(transactions);
+                        // var checkingBalance = transactions.
+                        // int checkingBalance = 
+                        Console.WriteLine("");
+                        Console.WriteLine("Checking Account: ${totalChecking}");
+                        Console.WriteLine("Savings Account: ${}");
                         break;
+
+                    case "D":
+                        Console.WriteLine("");
+                        Console.WriteLine("Would you like to deposit funds to your [C]hecking or [S]avings account? ");
+                        var depositAccountChoice = Console.ReadLine().ToUpper();
+
+                        if (depositAccountChoice == "C")
+                        {
+                            var checkingDeposit = new Transaction();
+
+                            Console.WriteLine("");
+                            checkingDeposit.Date = DateTime.Now;
+                            checkingDeposit.Account = "Checking";
+                            checkingDeposit.Type = "Deposit";
+                            checkingDeposit.Amount = PromptForInteger("How much would you like to deposit into your checking account? ");
+                            Console.WriteLine("");
+                            Console.WriteLine($"${checkingDeposit.Amount} has been deposited into your checking account. ");
+
+                            transactions.Add(checkingDeposit);
+                        }
+                        else if (depositAccountChoice == "S")
+                        {
+                            var savingsDeposit = new Transaction();
+
+                            Console.WriteLine("");
+                            savingsDeposit.Date = DateTime.Now;
+                            savingsDeposit.Account = "Savings";
+                            savingsDeposit.Type = "Deposit";
+                            savingsDeposit.Amount = PromptForInteger("How much would you like to deposit into your savings account? ");
+                            Console.WriteLine("");
+                            Console.WriteLine($"${savingsDeposit.Amount} has been deposited into your savings account. ");
+
+                            transactions.Add(savingsDeposit);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("");
+                            Console.WriteLine("❗That is not a valid selection. Try again❗");
+                        }
+
+
+
+                        break;
+
+                    case "W":
+                        break;
+
+                    case "T":
+                        break;
+
+                    case "Q":
+                        keepGoing = false;
+                        break;
+                    // VIEW ACCOUNT BALANCES.
+                    // If user selects `deposit to savings`
+                    // - prompt user to input amount (PromptForInteger)
+                    // - add to savings history
+                    // - update balance
+                    // If user selects `deposit to checking`
+                    // - prompt user to input amount (PromptForInteger)
+                    // - add to checking history
+                    // - update balance
+                    // If user selects `withdraw from savings`
+                    // - prompt user to input amount (PromptForInteger)
+                    // - add amount to savings history
+                    // - update balance
+                    //   - if withdrawal would make balance < 0, display error message
+                    // If user selects `withdraw from checking`
+                    // - prompt user to input amount (PromptForInteger)
+                    // - add to checking history
+                    // - update balance
+                    //   - if withdrawal would make balance < 0, display error message
+
 
                     default:
                         Console.ForegroundColor = ConsoleColor.Red;
---
+++ Transaction.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace FirstBankOfSuncoast
+{
+    class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string Account { get; set; }
+        public string Type { get; set; }
+        public int Amount { get; set; }
+    }
+}
+// ALGORITHM
+// Load past transactions from csv file.
+
+
+
